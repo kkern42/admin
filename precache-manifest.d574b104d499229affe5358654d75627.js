20045dc05f234c5be18ddafe90808d73e52def91
Kevin Kern <kjk9cd@virginia.edu>
2020-05-31 21:46:15 -0400

    Updates

---
+++ precache-manifest.d574b104d499229affe5358654d75627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2115d72a64abecbde196a141f59ac46",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "4a06d90878145c28f134",
+    "url": "/admin/static/css/main.849ade25.chunk.css"
+  },
+  {
+    "revision": "2f36c5d0c974f637ca3a",
+    "url": "/admin/static/js/2.6b97b8c6.chunk.js"
+  },
+  {
+    "revision": "2954b1568600ec1d7b70d079ce9ac1a4",
+    "url": "/admin/static/js/2.6b97b8c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a06d90878145c28f134",
+    "url": "/admin/static/js/main.2d4b2354.chunk.js"
+  },
+  {
+    "revision": "f11da11adfcc55fc06b2",
+    "url": "/admin/static/js/runtime-main.303e1753.js"
+  }
+]);
